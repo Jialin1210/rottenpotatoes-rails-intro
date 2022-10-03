4a2b7735b58ecaeb98071e528818ac6d35ec6815
Lynn Zhu <jz2969@columbia.edu>
2022-10-03 16:22:57 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+  def self.with_ratings(ratings_list)
+    if ratings_list.empty?
+      Movie.all
+    else
+      Movie.where(rating: ratings_list)
+    end
+  end
+  
+  def self.all_ratings()
+     ['G','PG','PG-13','R']
+  end
+  
+  def self.sort_by(means)
+    Movie.order(means)
+  end
+    
 end
